6e503ac415064725a4b2530f79396691fdf1b2b8
danilomassoni <massonidanilo@gmail.com>
2025-05-05 22:27:18 -0300

    Fixed the login jwt

--- src/main/java/com/hawkscheck/hawkscheck/config/WebConfig.java
+++ src/main/java/com/hawkscheck/hawkscheck/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         // Configura o CORS para permitir requisições do frontend
-        registry.addMapping("/api/**")  // Aplique o CORS para todas as rotas que começam com "/api"
+        registry.addMapping("/**")  // Aplique o CORS para todas as rotas que começam com "/api"
                 .allowedOrigins("http://localhost:5173")  // Permite o acesso do frontend rodando em http://localhost:5173
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // Permite os métodos HTTP
                 .allowedHeaders("Content-Type", "Authorization")  // Permite os cabeçalhos necessários
