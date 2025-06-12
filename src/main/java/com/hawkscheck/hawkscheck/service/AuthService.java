050998875745453e4e73e8964386771e71f876e3
danilomassoni <massonidanilo@gmail.com>
2025-06-11 22:44:42 -0300

    Add comment

---
+++ src/main/java/com/hawkscheck/hawkscheck/service/AuthService.java
@@ -0,0 +1,17 @@
+package com.hawkscheck.hawkscheck.service;
+
+import com.hawkscheck.hawkscheck.dto.AuthRequestDTO;
+import com.hawkscheck.hawkscheck.dto.AuthResponseDTO;
+import com.hawkscheck.hawkscheck.dto.UserRequestDTO;
+import com.hawkscheck.hawkscheck.model.User;
+import com.hawkscheck.hawkscheck.repository.UserRepository;
+import com.hawkscheck.hawkscheck.security.JwtService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+//Refatorando
+public class AuthService {
+    
+}
