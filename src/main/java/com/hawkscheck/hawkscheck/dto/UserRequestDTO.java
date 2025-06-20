bd59060bf7ea5f36fb07fac25dbad48a6e1fadba
danilomassoni <massonidanilo@gmail.com>
2025-06-20 18:34:54 -0300

    Add UserControllerTest and repaior the UserRequestDTO

--- src/main/java/com/hawkscheck/hawkscheck/dto/UserRequestDTO.java
+++ src/main/java/com/hawkscheck/hawkscheck/dto/UserRequestDTO.java
@@ -2,9 +2,13 @@ package com.hawkscheck.hawkscheck.dto;
 
 import com.hawkscheck.hawkscheck.model.PaperEnum;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRequestDTO {
     private String name;
     private String email;
